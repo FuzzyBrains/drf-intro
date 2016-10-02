721f8f508ed43366804c37cca005b1f0486c480b
Andrea Stagi <stagi.andrea@gmail.com>
2016-10-02 12:41:05 +0200

    Make Talk topic field unique

--- api/models.py
+++ api/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 
 class Talk(models.Model):
-	topic = models.CharField(max_length=200)
+	topic = models.CharField(max_length=200, unique=True)
 	votes = models.IntegerField(default=0)
